0e2d3b88f4109712f92c07ef14ee9ea3711ce2a6
Adnan Hoq <adnanbinhoq@gmail.com>
2018-12-22 23:08:09 -0500

    Update settings.py

--- aweb/aweb/settings.py
+++ aweb/aweb/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE_CLASSES = [
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "prithynosheensoitan@gmail.com"
-EMAIL_HOST_PASSWORD = 'prithynosheen69'
+EMAIL_HOST_PASSWORD = 'Prithynosheen69!'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 
