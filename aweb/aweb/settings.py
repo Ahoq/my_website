9da8cef72f7693586ffb9cf47bf2476d86329990
Ahoq <adnanbinhoq@gmail.com>
2018-12-22 22:13:37 -0500

    email functionality added1

--- aweb/aweb/settings.py
+++ aweb/aweb/settings.py
@@ -54,10 +54,10 @@ MIDDLEWARE_CLASSES = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
+EMAIL_PORT=8080
 EMAIL_HOST_USER = 'prithynosheensoitan@gmail.com'
 EMAIL_HOST_PASSWORD = 'prithynosheen69'
-EMAIL_USE_TLS = True
+
 
 
 ROOT_URLCONF = 'aweb.urls'
