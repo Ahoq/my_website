afb60f64db55e08a584d25b31715498e759ff5ac
Ahoq <adnanbinhoq@gmail.com>
2018-12-22 23:33:53 -0500

    email settings changed5

--- aweb/website/views.py
+++ aweb/website/views.py
@@ -14,11 +14,12 @@ def email(request):
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['ahoqabir@gmail.com'])
-                send_mail(subject, from_email, from_email, ['ahoqabir@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
+            send_mail(subject, message, from_email, ['ahoqabir@gmail.com'])
+            #try:
+                #send_mail(subject, message, from_email, ['ahoqabir@gmail.com'])
+                #send_mail(subject, from_email, from_email, ['ahoqabir@gmail.com'])
+            #except BadHeaderError:
+                #return HttpResponse('Invalid header found.')
             return redirect('thanks')
     return render(request, "website/email.html/", {'form': form})
 
